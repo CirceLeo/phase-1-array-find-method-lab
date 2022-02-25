6380ebde73b08448c2621535ded47c91ca8669bf
Calypso Leonard <calypsorleonard@gmail.com>
2022-02-25 10:40:47 -0500

    commit tree 41fcd93512d7142b937071b8b11a9062def3ad8b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+// const record = [
+//     { year: "2015", result: "d"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+// ]
+// function superbowlWin(array){
+//     let result 
+//     if (result = array.find(isWin)){
+//         return result.year
+//     }
+//     return result
+// }
+
+function superbowlWin(array){
+    let result 
+    if (result = array.find(record => record.result === "W")){
+        return result.year
+    }
+    return result
+}
+
+function isWin(element){
+    return (element.result === "W");
+}
